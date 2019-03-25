27b29b38ce63bda78cd64ae9836bd47dd92b3d2f
dosecin <417745982@qq.com>
2019-03-25 15:42:32 +0800

    两数相加

---
+++ 2.两数相加.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=golang
+ *
+ * [2] 两数相加
+ *
+ * https://leetcode-cn.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (33.47%)
+ * Total Accepted:    102.7K
+ * Total Submissions: 306.9K
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var head *ListNode
+	pAdd := &head
+	carry := false
+
+	for l1 != nil || l2 != nil {
+		var val int
+		if l1 != nil {
+			val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			val += l2.Val
+			l2 = l2.Next
+		}
+
+		if carry {
+			val++
+			carry = false
+		}
+
+		if val > 9 {
+			carry = true
+			val -= 10
+		}
+
+		*pAdd = &ListNode{val, nil}
+		pAdd = &((*pAdd).Next)
+	}
+
+	if carry {
+		*pAdd = &ListNode{1, nil}
+	}
+
+	return head
+}
