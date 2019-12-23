29654620f15f1c96877bbdd6cdf68378ceef5c62
dosecin <dosecin@163.com>
2019-12-23 08:56:11 +0800

    array

--- 780.到达终点.go
+++ 780.到达终点.go
@@ -7,10 +7,21 @@
 // @lc code=start
 func reachingPoints(sx int, sy int, tx int, ty int) bool {
 	for tx >= sx && ty >= sy {
+		if tx == ty {
+			break
+		}
 		if tx > ty {
-			tx = tx - ty
+			if ty > sy {
+				tx %= ty
+			} else {
+				return (tx-sx)%ty == 0
+			}
 		} else {
-			ty = ty - tx
+			if tx > sx {
+				ty %= tx
+			} else {
+				return (ty-sy)%tx == 0
+			}
 		}
 	}
 	return sx == tx && sy == ty
